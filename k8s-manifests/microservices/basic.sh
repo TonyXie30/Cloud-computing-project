6493f89f085b3f2925eb9472d097dca70b4e9ce7
honhaoyuan0 <promc1314@gmail.com>
2024-07-17 15:05:51 +0800

    Added rollingupdate strategy and scaled redis replica from 1 to 2

--- k8s-manifests/microservices/basic.sh
+++ k8s-manifests/microservices/basic.sh
@@ -5,11 +5,7 @@ kubectl apply -f ./auth/configmap-auth.yaml
 kubectl apply -f ./main/configmap-main.yaml
 kubectl apply -f ./user/configmap-user.yaml
 kubectl apply -f ./verify/configmap-verify.yaml
-# secrets
-kubectl apply -f ./secret/secret-auth.yaml
-kubectl apply -f ./secret/secret-main.yaml
-kubectl apply -f ./secret/secret-user.yaml
-kubectl apply -f ./secret/secret-verify.yaml
+
 # Storage
 kubectl apply -f ./Storage/ebs-sc.yaml
 kubectl apply -f ./main/statefulset-main.yaml
